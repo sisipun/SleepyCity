6a8c493cc5d40271ea9dd6ef067e5cd4aece79e4
Sisipun <sisipun@gmail.com>
2021-04-03 13:32:07 +0300

    Max alive count and start timer ater alive set

--- Cell.gd
+++ Cell.gd
@@ -1,25 +1,33 @@
 extends Area2D
 
+signal clicked(coord_x, coord_y)
 export var alive = true
 
 var alive_texture = preload("res://alive_cell.png")
 var dead_texture = preload("res://dead_cell.png")
+var coord_x = 0
+var coord_y = 0
 
-func _ready():
+func init(coord_x, coord_y, alive, position, size):
+	self.coord_x = coord_x
+	self.coord_y = coord_y
+	self.position = position
 	set_alive(alive)
-		
-func set_alive(new_alive):
-	alive = new_alive
-	if alive:
-		$Sprite.set_texture(alive_texture)
-	else:
-		$Sprite.set_texture(dead_texture)
-
-func set_size(size):
 	var current_size = $Sprite.texture.get_size()
 	$Collision.shape.extents = Vector2(size.x / 2, size.y / 2)
 	$Sprite.scale = Vector2(size.x / current_size.x, size.y / current_size.y)
+	return self
 
-func _on_Cell_input_event(viewport, event, shape_idx):
+func _ready():
+	set_alive(alive)
+
+func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
-        set_alive(!alive)
+		emit_signal("clicked", self.coord_x, self.coord_y)
+
+func set_alive(alive):
+	self.alive = alive
+	if self.alive:
+		$Sprite.set_texture(alive_texture)
+	else:
+		$Sprite.set_texture(dead_texture)
--- Main.gd
+++ Main.gd
@@ -8,9 +8,11 @@ export var born_condition = [3]
 export var survive_condition = [2, 3]
 export var step_count = 5
 
+var user_map = []
 var map = []
 var screen_size
 var step = 0
+var alive_count = 0
 
 func _ready():
 	screen_size = get_viewport_rect().size	
@@ -18,33 +20,55 @@ func _ready():
 	var cell_height = screen_size.y / height
 	for i in range(width):
 		map.append([])
+		user_map.append([])
 		for j in range(height):
-			var cell = Cell.instance()
+			var cell = Cell.instance().init(i, j, false, Vector2(cell_width / 2 + i * cell_width, cell_height / 2 + j * cell_height), Vector2(cell_width, cell_height))
+			cell.connect("clicked", self, "_on_Cell_clicked")
 			add_child(cell)
-			cell.set_alive(false)
-			cell.set_size(Vector2(cell_width, cell_height))
-			cell.position = Vector2(cell_width / 2 + i * cell_width, cell_height / 2 + j * cell_height)
 			map[i].append(cell)
-	#$StepTimer.start()
+			user_map[i].append(cell.alive)
 
 func _process(delta):
-	if (Input.is_action_just_pressed("ui_left")):
-		step()
+	if Input.is_action_just_pressed("ui_left") and alive_count == alive_max_count:
+		$StepTimer.start()
+		
+func _on_StepTimer_timeout():
+	step()
+	step += 1
+	if step >= step_count:
+		$StepTimer.stop()
+		step = 0
+		reset_map()
+
+func _on_Cell_clicked(coord_x, coord_y):
+	var cell = map[coord_x][coord_y]
+	if not cell.alive and alive_count < alive_max_count:
+		alive_count += 1
+		cell.set_alive(true)
+	elif cell.alive:
+		alive_count -= 1
+		cell.set_alive(false)
+	user_map[coord_x][coord_y] = cell.alive
 
 func step():
-	var new_statuses = [] 
+	var new_statuses = []
 	for i in range(map.size()):
 		new_statuses.append([])
 		for j in range(map[i].size()):
 			var alive = map[i][j].alive
-			var alive_count = alive_count(i, j)
-			new_statuses[i].append((alive and alive_count in survive_condition) or (not alive and alive_count in born_condition))
+			var alive_around_count = alive_around_count(i, j)
+			new_statuses[i].append((alive and alive_around_count in survive_condition) or (not alive and alive_around_count in born_condition))
 	
 	for i in range(new_statuses.size()):
 		for j in range(new_statuses[i].size()):
 			map[i][j].set_alive(new_statuses[i][j])	
-			
-func alive_count(i, j):
+
+func reset_map():
+	for i in range(map.size()):
+		for j in range(map[i].size()):
+			map[i][j].set_alive(user_map[i][j])	
+	
+func alive_around_count(i, j):
 	var inc_i = i + 1 if map.size() > i + 1 else 0
 	var inc_j = j + 1 if map[i].size() > j + 1 else 0
 	var counter = 0
@@ -65,9 +89,3 @@ func alive_count(i, j):
 	if map[inc_i][j - 1].alive:
 		counter += 1
 	return counter
-
-func _on_StepTimer_timeout():
-	step()
-	step += 1
-	if step >= step_count:
-		$StepTimer.stop()
