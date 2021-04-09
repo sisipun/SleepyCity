5866f973f6ef0dbfa1b5821df65f4e49bfe9fe98
Sisipun <sisipun@gmail.com>
2021-04-09 19:22:48 +0300

    Level complete

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -7,52 +7,45 @@ enum Stage {
 	PREVIEW
 }
 
-export var width = 20
-export var height = 10
-export var alive_max_count = 3
-export var born_condition = [3]
-export var survive_condition = [2, 3]
-export var step_count = 1
-export var targets = [Vector2(4,4), Vector2(4,5), Vector2(3,4), Vector2(3,5)]
-
-var screen_size
-var stage = Stage.USER_INPUT
-var map = []
-var user_input_map = []
-var step_number = 0
-var alive_count = 0
+var level
+var stage
+var map
+var user_input_map
+var step_number
+var alive_count
 
 func _ready():
-	var levelInfo = Levels.current()
-	self.width = levelInfo.width
-	self.height = levelInfo.height
-	self.alive_max_count = levelInfo.alive_max_count
-	self.born_condition = levelInfo.born_condition
-	self.survive_condition = levelInfo.survive_condition
-	self.step_count = levelInfo.step_count
-	self.targets = levelInfo.targets
+	var cellScene = load("res://scenes/Cell.tscn")	
+	level = Levels.current()
+	stage = Stage.USER_INPUT	
+	map = []
+	user_input_map = []
+	step_number = 0
+	alive_count = 0
 	
-	var cellScene = load("res://scenes/Cell.tscn")
-	screen_size = get_viewport_rect().size
-	stage = Stage.USER_INPUT
-	var cell_width = screen_size.x / width
-	var cell_height = screen_size.y / height
-	for i in range(width):
+	var screen_size = get_viewport_rect().size
+	var cell_width = screen_size.x / level.width
+	var cell_height = screen_size.y / level.height
+	for i in range(level.width):
 		map.append([])
 		user_input_map.append([])
-		for j in range(height):
-			var cell = cellScene.instance().init(i, j, Vector2(cell_width / 2 + i * cell_width, cell_height / 2 + j * cell_height), Vector2(cell_width, cell_height))
+		for j in range(level.height):
+			var cell = cellScene.instance().init(
+				i, 
+				j, 
+				Vector2(cell_width / 2 + i * cell_width, cell_height / 2 + j * cell_height), 
+				Vector2(cell_width, cell_height)
+			)
 			cell.connect("clicked", self, "_on_Cell_clicked")
 			add_child(cell)
 			map[i].append(cell)
 			user_input_map[i].append(cell.is_alive())
 			
-	targets.sort()	
-	for target in targets:
+	for target in level.targets:
 		var cell = cellScene.instance().init(target.x, target.y, Vector2(cell_width / 2 + target.x * cell_width, cell_height / 2 + target.y * cell_height), Vector2(cell_width, cell_height), 0.5, Cell.Type.TARGET)
 		add_child(cell)
 	
-	$HUD/ActiveCount.text = "Active count %d/%d" % [alive_count, alive_max_count]
+	$HUD/ActiveCount.text = "Active count %d/%d" % [alive_count, level.alive_max_count]
 	$HUD/ActiveCount.show()
 
 func _on_Cell_clicked(coord_x, coord_y):
@@ -60,7 +53,7 @@ func _on_Cell_clicked(coord_x, coord_y):
 		return
 	
 	var cell = map[coord_x][coord_y]
-	if not cell.is_alive() and alive_count < alive_max_count:
+	if not cell.is_alive() and alive_count < level.alive_max_count:
 		alive_count += 1
 		cell.set_alive(true)
 	elif cell.is_alive():
@@ -68,12 +61,12 @@ func _on_Cell_clicked(coord_x, coord_y):
 		cell.set_alive(false)
 
 	user_input_map[coord_x][coord_y] = cell.is_alive()	
-	if alive_count == alive_max_count:
+	if alive_count == level.alive_max_count:
 		$HUD/ActiveCount.hide()
 		$HUD/PreviewButton.show()
 	else:
 		$HUD/PreviewButton.hide()
-		$HUD/ActiveCount.text = "Active count %d/%d" % [alive_count, alive_max_count]
+		$HUD/ActiveCount.text = "Active count %d/%d" % [alive_count, level.alive_max_count]
 		$HUD/ActiveCount.show()
 
 func _on_PreviewButton_pressed():
@@ -81,21 +74,21 @@ func _on_PreviewButton_pressed():
 		return
 		
 	stage = Stage.PREVIEW
-	$HUD/StepNumber.text = "Step: %d/%d" % [step_number, step_count]
+	$HUD/StepNumber.text = "Step: %d/%d" % [step_number, level.step_count]
 	$HUD/StepNumber.show()
 	$StepTimer.start()
 
 func _on_StepTimer_timeout():
 	step_number += 1
-	if step_number > step_count:
-		$StepTimer.stop()
-		step_number = 0
-		if not is_target_complete():
-			stage = Stage.USER_INPUT
-			reset_map()
-	else: 
+	if step_number <= level.step_count:
 		step()
-		$HUD/StepNumber.text = "Step: %d/%d" % [step_number, step_count]
+		$HUD/StepNumber.text = "Step: %d/%d" % [step_number, level.step_count]
+		return
+		
+	if is_target_complete():
+		complete()
+	else:
+		reset()
 
 func step():
 	var new_statuses = []
@@ -104,18 +97,24 @@ func step():
 		for j in range(map[i].size()):
 			var alive = map[i][j].is_alive()
 			var alive_around_count = alive_around_count(i, j)
-			new_statuses[i].append((alive and alive_around_count in survive_condition) or (not alive and alive_around_count in born_condition))
+			new_statuses[i].append((alive and alive_around_count in level.survive_condition) or (not alive and alive_around_count in level.born_condition))
 	
 	for i in range(new_statuses.size()):
 		for j in range(new_statuses[i].size()):
 			map[i][j].set_alive(new_statuses[i][j])
 
-func reset_map():
+func reset():
+	$StepTimer.stop()
+	step_number = 0
+	stage = Stage.USER_INPUT
 	for i in range(map.size()):
 		for j in range(map[i].size()):
 			map[i][j].set_alive(user_input_map[i][j])
 	
 	$HUD/StepNumber.hide()
+
+func complete():
+	get_tree().change_scene("res://scenes/ChooseLevel.tscn")
 	
 func alive_around_count(i, j):
 	var inc_i = i + 1 if map.size() > i + 1 else 0
@@ -146,4 +145,4 @@ func is_target_complete():
 			if map[i][j].is_alive():
 				active_cells.append(Vector2(i, j))
 	active_cells.sort()
-	return active_cells == targets
+	return active_cells == level.targets
--- scripts/Levels.gd
+++ scripts/Levels.gd
@@ -1,10 +1,32 @@
 extends Node
 
+class LevelInfo:
+	var width
+	var height
+	var alive_max_count
+	var born_condition
+	var survive_condition
+	var step_count
+	var targets
+
+	func _init(width, height, alive_max_count, born_condition, survive_condition, step_count, targets):
+		self.width = width
+		self.height = height
+		self.alive_max_count = alive_max_count
+		self.born_condition = born_condition
+		self.survive_condition = survive_condition
+		self.step_count = step_count
+		self.targets = targets
+
 var values = [
 	LevelInfo.new(20, 10, 3, [3], [2,3], 1, [Vector2(4,4), Vector2(4,5), Vector2(3,4), Vector2(3,5)]),
 	LevelInfo.new(20, 10, 3, [3], [2,3], 1, [Vector2(4,4), Vector2(4,5), Vector2(4,6)])
 ]
 var current = 0
+
+func _ready():
+	for level in values:
+		level.targets.sort()
 
 func current():
 	return values[current]
