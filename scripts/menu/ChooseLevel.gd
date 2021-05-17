0bf29d722cc2fbfd53e5ff3bae270630bd6a8b93
Sisipun <sisipun@gmail.com>
2021-05-17 20:54:37 +0300

    Choose level container

--- scripts/menu/ChooseLevel.gd
+++ scripts/menu/ChooseLevel.gd
@@ -1,15 +1,11 @@
-extends Control
+extends VBoxContainer
 
 func _ready():
 	var preview_scene = load("res://scenes/menu/LevelPreview.tscn")
-	var horizontal_container
 	for i in len(Game.levels()):
-		if i % 2 == 0:
-			horizontal_container = HBoxContainer.new()
-			$ButtonsScroll/Buttons.add_child(horizontal_container)
 		var preview = preview_scene.instance().init(i)
 		preview.connect("clicked", self, "_on_level_clicked")
-		horizontal_container.add_child(preview)
+		$ButtonsScroll/ButtonsMargin/Buttons.add_child(preview)
 
 func _on_level_clicked(i):
 	Game.currentLevelIndex = i
--- scripts/menu/LevelPreview.gd
+++ scripts/menu/LevelPreview.gd
@@ -5,20 +5,22 @@ signal clicked(index)
 var play_texture = load("res://assets/preview/play.png")
 var disabled_texture = load("res://assets/preview/disabled.png")
 
-var level_index = 0
+var level_index
+var disabled
 
 func init(index):
-	level_index = index
-	var level = Game.levels()[level_index]
-	$PreviewArea/Bottom/LevelLabel.text = "%d" % (level_index + 1)
+	var level = Game.levels()[index]
 	
-	if level.opened:
-		$PreviewArea/Bottom/LevelLabel.set_modulate(Color(0, 0, 0, 1))
-		$PreviewTexture.texture = play_texture
-	else:
+	level_index = index
+	disabled = not level.opened
+	
+	$PreviewArea/Bottom/LevelLabel.text = "%d" % (level_index + 1)
+	if disabled:
 		$PreviewArea/Bottom/LevelLabel.set_modulate(Color(1, 1, 1, 1))
 		$PreviewTexture.texture = disabled_texture
-	
+	else:
+		$PreviewArea/Bottom/LevelLabel.set_modulate(Color(0, 0, 0, 1))
+		$PreviewTexture.texture = play_texture
 	if level.bonus:
 		$PreviewArea/Bottom/BonusTexture.show()
 	else:
@@ -27,6 +29,6 @@ func init(index):
 	return self
 
 func _on_gui_input(event):
-	if event is InputEventScreenTouch:
+	if event is InputEventScreenTouch and not disabled:
 		if event.is_pressed():
 			emit_signal("clicked", level_index)
