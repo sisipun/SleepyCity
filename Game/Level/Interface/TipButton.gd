e7cd3c5fc1e3767dad40b64a880768ece9c108ea
Sisipun <sisipun@gmail.com>
2021-05-24 20:43:37 +0300

    new levels

--- Game/Level/Interface/TipButton.gd
+++ Game/Level/Interface/TipButton.gd
@@ -2,8 +2,12 @@ extends Button
 
 
 func _ready() -> void:
+	if Game.tip_count() <= 0:
+		disabled = true
 	text = "Tip (%d)" % Game.tip_count()
 
 
 func _on_pressed() -> void:
+	if Game.tip_count() <= 0:
+		disabled = true
 	text = "Tip (%d)" % Game.tip_count()
