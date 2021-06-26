f4a8963a9d044ca7333fdfcf491c48dafab0fe10
Sisipun <sisipun@gmail.com>
2021-06-26 17:33:11 +0300

    Main menu buttons

---
+++ Game/Menu/Common/LinkTextureButton.gd
@@ -0,0 +1,7 @@
+extends Node
+
+export (String) var link_path
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene(link_path)
--- Game/Menu/MainMenu/SoundButtons.gd
+++ Game/Menu/MainMenu/SoundButtons.gd
@@ -1,31 +1,31 @@
 extends HBoxContainer
 
 
-func _ready() -> void:
-	if Storage.has_sound():
-		$SoundButton.text = "Sound off"
-	else:
-		$SoundButton.text = "Sound on"
+#func _ready() -> void:
+	#if Storage.has_sound():
+		#$SoundButton.text = "Sound off"
+	#else:
+		#$SoundButton.text = "Sound on"
 		
-	if Storage.has_music():
-		$MusicButton.text = "Music off"
-	else:
-		$MusicButton.text = "Music on"
+	#if Storage.has_music():
+		#$MusicButton.text = "Music off"
+	#else:
+		#$MusicButton.text = "Music on"
 
 
 func _on_sound_pressed() -> void:
 	if Storage.has_sound():
 		Storage.mute_sound()
-		$SoundButton.text = "Sound on"
+		#$SoundButton.text = "Sound on"
 	else:
 		Storage.unmute_sound()
-		$SoundButton.text = "Sound off"
+		#$SoundButton.text = "Sound off"
 
 
 func _on_music_pressed() -> void:
 	if Storage.has_music():
 		Storage.mute_music()
-		$MusicButton.text = "Music on"
+		#$MusicButton.text = "Music on"
 	else:
 		Storage.unmute_music()
-		$MusicButton.text = "Music off"
+		#$MusicButton.text = "Music off"
