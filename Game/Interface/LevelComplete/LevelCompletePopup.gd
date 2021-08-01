44f7db448509217e25d719b75fb5a3f20ebad031
Sisipun <sisipun@gmail.com>
2021-08-01 11:03:02 +0300

    Show stars as icons instead of text

--- Game/Interface/LevelComplete/LevelCompletePopup.gd
+++ Game/Interface/LevelComplete/LevelCompletePopup.gd
@@ -5,19 +5,25 @@ class_name LevelCompletePopup
 
 
 export (NodePath) onready var _level_label = get_node(_level_label) as Label
-export (NodePath) onready var _step_label = get_node(_step_label) as Label
 export (NodePath) onready var _bonus_icon = get_node(_bonus_icon) as NinePatchRect
+export (Array, NodePath) var _stars
 
+
+var _star_icons: Array = []
 
 func _ready() -> void:
 	EventStorage.connect("level_completed", self, "_on_level_completed")
+	for _star_path in _stars:
+		_star_icons.push_back(get_node(_star_path) as NinePatchRect)
 
 
 func _on_level_completed(level: LevelInfo, level_number: int, step_count: int, stars_count: int, earned_bonus: bool) -> void:
 	popup_centered()
 	
 	_level_label.text = str(level_number)
-	_step_label.text = str(stars_count)
+	
+	for i in range(stars_count):
+		_star_icons[i].show()
 	
 	if earned_bonus:
 		_bonus_icon.show()
