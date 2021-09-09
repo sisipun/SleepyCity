fbf79f6888920355e1ad99d4e96093173c3e294f
Sisipun <sisipun@gmail.com>
2021-09-09 19:03:57 +0300

    Revarded ad on skip level

--- Game/Interface/Interface.gd
+++ Game/Interface/Interface.gd
@@ -4,6 +4,20 @@ extends Control
 class_name Interface
 
 
+func _ready() -> void:
+	MobileAds.connect("initialization_complete", self, "_on_ads_initialization_complete")
+	MobileAds.connect("rewarded_ad_closed", self, "_on_ads_rewarded_ad_closed")
+
+
+func _on_ads_initialization_complete(status, _adapter_name) -> void:
+	if status == MobileAds.INITIALIZATION_STATUS.READY:
+		MobileAds.load_rewarded()
+
+
+func _on_ads_rewarded_ad_closed() -> void:
+	MobileAds.load_rewarded()
+
+
 func _on_reset_pressed() -> void:
 	EventStorage.emit_signal("reset")
 
@@ -17,6 +31,8 @@ func _on_menu_pressed() -> void:
 
 
 func _on_skip_pressed() -> void:
+	MobileAds.show_rewarded()
+	MobileAds.load_rewarded()
 	EventStorage.emit_signal("complete_current_level")
 
 
