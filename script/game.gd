0c93e1d0f5b4713b3a58962ebdd169c0a5680e1f
sisipun <sisipun@gmail.com>
2023-08-22 19:39:02 +0200

    Reinit remove

--- script/game.gd
+++
@@ -1,6 +0,0 @@
-class_name Game
-extends Node
-
-
-func _ready() -> void:
-	EventStorage.emit_signal("level_change_request", true)
--- script/game_state/resource/tutorial_resource.gd
+++
@@ -1,8 +0,0 @@
-class_name TutorialResource
-extends Resource
-
-
-@export var before: Texture2D
-@export var after: Texture2D
-@export var tap_x: int
-@export var tap_y: int
